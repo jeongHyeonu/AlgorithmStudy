e0481d59c98118445bf52f0f6541888149c655ab
정현우 <114675420+jeongHyeonu@users.noreply.github.com>
2024-08-20 14:49:41 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int T;
+int dp[12];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> T;
+
+	dp[1] = 1;
+	dp[2] = 2; 
+	dp[3] = 4; 
+	for (int i = 4;i < 11;i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
+
+	int n;
+	while (T--) {
+		cin >> n;
+		cout << dp[n] << '\n';
+	}
+}
